ce5649eaa2b0c8f4dab6ab0a8034f8a8629e1af4
rotemiara <39151776+rotemiara@users.noreply.github.com>
2020-07-15 09:24:06 +0300

    Update docs_helper.rb
    trying to fix url

--- app/helpers/apitome/docs_helper.rb
+++ app/helpers/apitome/docs_helper.rb
@@ -5,15 +5,15 @@ module Apitome::DocsHelper
     end.join
   end
 
-  def resource_link(resource)
-    "#{Apitome.configuration.mount_at}/#{CGI.escape(resource['examples'].first['link'].gsub(/\.json$/, ''))}"
-  end
+#   def resource_link(resource)
+#     "#{Apitome.configuration.mount_at}/#{CGI.escape(resource['examples'].first['link'].gsub(/\.json$/, ''))}"
+#   end
 
-  def example_link(example)
-    "#{Apitome.configuration.mount_at}/#{CGI.escape(example['link'].gsub(/\.json$/, ''))}"
-  end
+#   def example_link(example)
+#     "#{Apitome.configuration.mount_at}/#{CGI.escape(example['link'].gsub(/\.json$/, ''))}"
+#   end
 
-  def link_active?(link)
-    current_page?(url_for(link))
-  end
+#   def link_active?(link)
+#     current_page?(url_for(link))
+#   end
 end
